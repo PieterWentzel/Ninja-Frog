55ad968f19d599d1f8e3eb7cce366b28d5efa573
PieterWentzel <pietercarelwentzel@gmail.com>
2024-03-29 22:01:14 +0200

    Fixed double jump + add some template background music

--- Scripts/Main_Character.gd
+++ Scripts/Main_Character.gd
@@ -9,36 +9,37 @@ const JUMP_VELOCITY = -450
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var can_doubblejump = false
 
 func _physics_process(delta):
-	var can_doubblejump = false
+	
 	if (velocity.x > 1 || velocity.x < -1):
 		animated_sprite_2d.animation = "running"
 	else:
-		animated_sprite_2d.animation = "default"	
+		animated_sprite_2d.animation = "default"
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		animated_sprite_2d.animation = "jump"
-		can_doubblejump = true
-		
-		
-	if can_doubblejump and Input.is_action_just_pressed("jump") and not is_on_floor():
-		velocity.y += JUMP_VELOCITY
-		can_doubblejump = false
-		#animated_sprite_2d.animation = "DoubbleJump"
-	
-		
+	# Set falling animation
 	if velocity.y >= 1:
 		animated_sprite_2d.animation = "falling"
 	
+	if is_on_floor():
+		can_doubblejump = false	
+		
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		
 		velocity.y = JUMP_VELOCITY
+		can_doubblejump = true
 		
-	
+	# Handle doubble jump	
+	if can_doubblejump and Input.is_action_just_pressed("jump") and not is_on_floor():
+			velocity.y = JUMP_VELOCITY 
+			can_doubblejump = false
+					
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
@@ -47,9 +48,6 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, 16)
 	
-	
-		
-
 	move_and_slide()
 	
 	var isLeft = velocity.x < 0
--- Scripts/collectable_cherry.gd
+++ Scripts/collectable_cherry.gd
@@ -3,7 +3,9 @@ extends Area2D
 
 
 
+
 func _on_body_entered(body):
 	if (body.name == "CharacterBody2D"):
 		queue_free()
 		game_manager.add_point()
+		
